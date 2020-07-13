a5d075889c1294a570819bca9dff3495fb6702dc
timothy.kou <timothy.kou@iexcloud.io>
2020-07-13 18:50:38 -0400

    added docstrings to functions, added GridSearch hyperparameter tuning

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -23,6 +23,14 @@ from sklearn.svm import LinearSVC
 def load_data(database_filepath):
     '''
     Load dataset from db, define feature and target variables X and Y
+
+    Args: 
+    database_filepath - path to load dataframe from
+    
+    Returns:
+    X - features dataset
+    y - responses dataset
+    list(df.columns) - list of columns names corresponding to features in X
     '''
     engine = create_engine(f'sqlite:///{database_filepath}')
     query = '''
@@ -40,10 +48,20 @@ def load_data(database_filepath):
     X = df.message.values
     df = df.drop(columns=['message', 'id', 'original', 'genre'])
     y = df.apply(pd.to_numeric).values
+    
     return X, y, list(df.columns)
 
 
 def tokenize(text):
+    '''
+    Tokenizes text using NLTK's word_tokenize and WordNetLemmatizer
+
+    Args:
+    text - string of sentence(s) to be tokenized
+
+    Returns:
+    clean_tokens - list of tokens
+    '''
     # tokenize words
     tokens = word_tokenize(text)
     # lemmatize words 
@@ -59,7 +77,10 @@ def tokenize(text):
     return clean_tokens
 
 
-def build_model():
+def build_model(parameters=None):
+    '''
+    Creates a pipeline with CountVerctorizer(), TfidfTransformer(), and LinearSVC()
+    '''
     pipeline = Pipeline([
         ('vect', CountVectorizer(
             tokenizer=tokenize,
@@ -69,21 +90,36 @@ def build_model():
         ('tfidf', TfidfTransformer(
             use_idf=True,
         )),
-        ('clf', MultiOutputClassifier(LinearSVC()))
-        
+        ('clf', MultiOutputClassifier(LinearSVC(verbose=1)))
     ])
     print(pipeline.get_params())
-    # return pipeline
-    parameters = {
-    }
+
+    # grid search parameters or use params in args (best params)
+    if not parameters:
+        parameters = {
+            'vect__ngram_range': [(1,3), (1,2), (1,4)],
+            'vect__max_df': [0.5, 1.0],
+            'clf__estimator__loss': ['hinge', 'squared_hinge'],
+        }
+
     cv = GridSearchCV(pipeline, param_grid=parameters, verbose=1)
     return cv
 
 
 def evaluate_model(model, X_test, y_test, category_names, database_filepath):
+    '''
+    Evaluates model accuracy for each predicted category 
 
-    
-    # model.fit(X_train, y_train)
+    Args:
+    model - trained model created by build_model()
+    X_test - test portion of features dastaset
+    y_test - test portion of responses dataset
+    category_names - list of categories corresponding to columns in X_test
+    database_filepath - path to a db to save the accuracy results of the model
+
+    Returns:
+    None
+    '''
     y_pred = model.predict(X_test)
 
     print(f'\nBest params: {model.best_params_}')
@@ -94,8 +130,9 @@ def evaluate_model(model, X_test, y_test, category_names, database_filepath):
     for ix, category in enumerate(category_names):
         report = classification_report(y_test[:,ix], y_pred[:,ix], output_dict=True)
         print(report)
-        accuracy_dict[category] = report['accuracy']
-        print(f'Saving accuracy score for {category}: {report['accuracy']}')
+        accuracy = report['accuracy']
+        accuracy_dict[category] = accuracy
+        print(f'Saving accuracy score for {category}: {accuracy}')
     
     engine = create_engine(f'sqlite:///{database_filepath}')
 
@@ -109,10 +146,27 @@ def evaluate_model(model, X_test, y_test, category_names, database_filepath):
         if_exists='replace'
     )
     print(f'Successfully saved accuracy_df to Accuracy_Scores table at {database_filepath}')
+    return
 
 
 def save_model(model, model_filepath):
-    pickle.dump(model, open(model_filepath, 'wb'))        
+    '''
+    Saves model as a pickle 
+    
+    Args:
+    model - trained model created in build_model()
+    model_filepath - filepath to save pickled model
+
+    Returns:
+    bool indicating whether model was successfully saved
+
+    '''
+    try:
+        pickle.dump(model, open(model_filepath, 'wb')) 
+        return True
+    except:
+        return False
+
 
 
 def main():
@@ -129,14 +183,20 @@ def main():
         print('Training model...')
         model.fit(X_train, y_train)
 
+        # train model with best params
+        print(f'\nBest params: {model.best_params_}')
+        best_params = model.best_params_
+        model = build_model(best_params)
+
         # predict on test data
         print('Evaluating model...')
         evaluate_model(model, X_test, y_test, category_names, database_filepath)
 
         print('Saving model...\n    MODEL: {}'.format(model_filepath))
-        save_model(model, model_filepath)
-
-        print('Trained model saved!')
+        if save_model(model, model_filepath):
+            print(f'Trained model saved to {model_filepath}')
+        else:
+            print(f'Could not save model to {model_filepath}')
 
     else:
         print('Please provide the filepath of the disaster messages database '\
